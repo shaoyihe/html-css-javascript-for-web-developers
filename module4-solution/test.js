a92e8076202b521d2db6407ca6850ab28dbd1707
shaoyihe <wubuqilai@gmail.com>
2016-09-11 21:38:56 +0800

    Module 4 Coding Assignment

---
+++ module4-solution/test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by heshaoyi on 9/11/16.
+ */
+
+"use strict";
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for(var index in names){
+    console.log(names[index]);
+}
